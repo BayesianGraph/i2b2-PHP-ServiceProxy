089147c0ae94ea61a2e51f0e4c9d7cd98f0650f7
James B Norman <james_norman@hms.harvard.edu>
2020-07-08 13:21:28 -0400

    Update index.php
    added parsing to remove the <?xml version="1" top node from all request and response XML messages.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /*  Harvard Medical School
  *  DBMI
  *  Weberlab
- *  
+ *
  *  Developed by James B. Norman
  *
  *  For classic ASP replacment of i2b2 server proxy of restful URLs
@@ -14,20 +14,34 @@
 //Debug your php sql sever dll install, you need to have sqlsrv in your PHP Stream
 //Registered PHP Streams	php, file, glob, data, http, ftp, zip, compress.zlib, https, ftps, phar, sqlsrv
 
+//Edit this function for your database connection string parameters
+function getdbconfig(){
+    return '{
+        "database": {
+        "host": "SERVERNAME",
+        "user": "UID",
+        "password": "PWD",
+        "dbname": "DBNAME"
+           }
+      }';
+}
+
 
 //Make sure that this is a POST request.
 header('Content-type: application/xml');
 
 if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
     //If it isn't a POST, send back a 405 Method Not Allowed header.
-    header($_SERVER["SERVER_PROTOCOL"]." 405 Method Not Allowed", true, 405);
+    header($_SERVER["SERVER_PROTOCOL"]." 405 adfMethod Not Allowed", true, 405);
     exit;
 }
 
 //Get the raw POST data from PHP's input stream.
 //This raw data should contain XML.
 $postData = trim(file_get_contents('php://input'));
+
 $rawxml = new SimpleXMLElement($postData);
+
 //Use internal errors for better error handling.
 libxml_use_internal_errors(true);
 
@@ -39,9 +53,9 @@ if($rawxml === false) {
     foreach(libxml_get_errors() as $xmlError) {
         echo $xmlError->message . "\n";
     }
+    echo "XML coult not be parsed";
     exit;
 }
-
 
 $authdata = new auth_data;
 $authdata->Request = (string)$rawxml->asXML();
@@ -49,20 +63,21 @@ $authdata->IPAddress = (string)$rawxml->message_header->proxy->client_ip;
 $authreturndata = authenticate($authdata);
 
 if($authreturndata->authenticated<>1){
+	//echo "sorry";
     echo auth_err_msg();
     die();
 }
 
 $clienturl = explode('/', $_SERVER['REQUEST_URI']);
 
+
 //build the input to the response function;
 $response_data = new response_data;
 $response_data->IPAddress = (string)$authdata->IPAddress;
 $response_data->Service = (string)$clienturl[count($clienturl) - 2];
 $response_data->Operation = (string)$clienturl[count($clienturl) - 1];
-$response_data->Request = (string)$rawxml->asXML();
+$response_data->Request = $rawxml->asXML();
 $response_data->UserID = (string)$authreturndata->username;
-
 header($_SERVER["SERVER_PROTOCOL"]." 200 OK", true, 200);
 echo getresponse($response_data);
 die();
@@ -83,10 +98,12 @@ function getresponse(response_data $rsp_data){
 
     $tsql_callSP = "{call HIVE.uspGetResponse(?,?,?,?,?)}";
 
+	$fixed = fix_xml($rsp_data->Request);
+
     $params = array(
         array($rsp_data->Service, SQLSRV_PARAM_IN),
         array($rsp_data->Operation, SQLSRV_PARAM_IN),
-        array($rsp_data->Request, SQLSRV_PARAM_IN),
+        array($fixed, SQLSRV_PARAM_IN),
         array($rsp_data->UserID, SQLSRV_PARAM_IN),
         array($rsp_data->IPAddress, SQLSRV_PARAM_IN)
     );
@@ -121,22 +138,29 @@ function getresponse(response_data $rsp_data){
 function authenticate(auth_data $authdata){
 	$auth = new auth_return;
 	$dbconfig = json_decode(getdbconfig());
-    $connectionInfo = array( "Database"=>$dbconfig->database->dbname, "UID"=>$dbconfig->database->user, "PWD"=>$dbconfig->database->password);
+	$connectionInfo = array( "Database"=>$dbconfig->database->dbname, "UID"=>$dbconfig->database->user, "PWD"=>$dbconfig->database->password);
     $conn = sqlsrv_connect( $dbconfig->database->host, $connectionInfo);
     if( $conn === false )
     {
-        $conn_err = "Could not connect.\n";
-        $conn_err += "<pre>";
-        $conn_err +=  print_r( sqlsrv_errors(), true);
-        $conn_err += "</pre>";
-        $auth->auth_err_msg = $conn_err;
+        //$conn_err = "Could not connect.\n";
+        //$conn_err += "<pre>";
+        //$conn_err +=  print_r( sqlsrv_errors(), true);
+        //$conn_err += "</pre>";
+
+		$auth->auth_err_msg = print_r( sqlsrv_errors(), true);
+        echo "<error>{$auth->auth_err_msg}</error>";
+		die();
+
+
+
     }
 
     $callSP = "{call PM.uspGetAuthenticate(?,?)}";
 
+	$fixed = fix_xml($authdata->Request);
 
     $params = array(
-        array($authdata->Request, SQLSRV_PARAM_IN),
+        array($fixed, SQLSRV_PARAM_IN),
         array($authdata->IPAddress, SQLSRV_PARAM_IN),
     );
 
@@ -155,6 +179,7 @@ function authenticate(auth_data $authdata){
 		$auth->trycustomauthenticate = $row["TryCustomAuthenticate"];
 
 	}
+
     if($auth->trycustomauthenticate !="0"){
 		//Put custom auth code here.
 	}
@@ -169,19 +194,12 @@ function auth_err_msg(){
 	$msg = "<ns2:response xmlns:ns2=\"http://www.i2b2.org/xsd/hive/msg/1.1/\" xmlns:ns4=\"http://www.i2b2.org/xsd/cell/pm/1.1/\" xmlns:ns3=\"http://www.i2b2.org/xsd/hive/msg/version/\" xmlns:tns=\"http://ws.pm.i2b2.harvard.edu\"><message_header><i2b2_version_compatible>1.1</i2b2_version_compatible><hl7_version_compatible>2.4</hl7_version_compatible><sending_application><application_name>PM Cell</application_name><application_version>1.601</application_version></sending_application><sending_facility><facility_name>i2b2 Hive</facility_name></sending_facility><receiving_application><application_name>PM Cell</application_name><application_version>1.601</application_version></receiving_application><receiving_facility><facility_name>i2b2 Hive</facility_name></receiving_facility><datetime_of_message>2012-02-21T20:19:50.732Z</datetime_of_message><message_control_id><message_num>0t24RR6mE87f55Af4RbdP</message_num><instance_num>1</instance_num></message_control_id><processing_id><processing_id>P</processing_id><processing_mode>I</processing_mode></processing_id><accept_acknowledgement_type>AL</accept_acknowledgement_type><application_acknowledgement_type>AL</application_acknowledgement_type><country_code>US</country_code><project_id>undefined</project_id></message_header><response_header><result_status><status type=\"ERROR\">Username or password does not exist</status></result_status></response_header></ns2:response>";
 	return $msg;
 }
-function getdbconfig(){
-
-    return '{
-        "database": {
-        "host": "SERVER NAME, PORT",
-        "user": "USER NAME",
-        "password": "PASSWORD",
-        "dbname": "DBNAME"
-           }
-      }';
+function fix_xml(string $data){
+    $temp = new SimpleXMLElement($data);
+    $dom = dom_import_simplexml($temp);
+    return (string)$dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
 
 }
-
 class auth_return{
 	public $err_num = "";
 	public $authenticated = "";
